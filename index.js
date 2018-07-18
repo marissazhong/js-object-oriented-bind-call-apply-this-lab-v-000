3d01cce26d7ab590d3e5515d7b96bc9e2f0082cf
Marissa Zhong <mwz2101@columbia.edu>
2018-07-18 12:12:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //Your code here
+
+function justInvoke(fn) {
+  const result = fn();
+  return result;
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, arg) {
+  return fn.apply(thisValue, arg);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  let fnCopied = functionToBeCopied.bind(thisValue);
+  return fnCopied;
+}
+
+// let fred;
+// let functionToBeCopied;
+// functionToBeCopied = function (){return this};
+// fred = { name: 'fred'};
+// console.log(returnNewFunctionOf(functionToBeCopied, fred));
